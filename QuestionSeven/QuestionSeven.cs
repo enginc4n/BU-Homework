51eaa38ad56910cd2ed08aeaf378117b4bf0e773
engin can <altinozengincan@gmail.com>
2022-11-25 13:53:52 +0300

    7. Question Added

---
+++ QuestionSeven/QuestionSeven.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace QuestionSeven
+{
+    class QuestionSeven
+    {
+        static void Main(string[] args)
+        {
+            int number, divider, remainder;
+
+            Console.WriteLine("Enter your number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your divider");
+            divider = Convert.ToInt32(Console.ReadLine());
+
+            if (divider > number)
+            {
+                remainder = number;
+            }
+            else if (divider == number)
+            {
+                remainder = 0;
+            }
+            else
+            {
+                remainder = number % divider;
+            }
+            Console.WriteLine($"Your remainder is {remainder}");
+        }
+    }
+}
